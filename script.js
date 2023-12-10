69d9d0b8a1b9db5e9da5e0982d197a33cc584c53
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-10 15:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const Div = document.querySelector(".search");
+const Btn = document.querySelector(".btn");
+const Input = document.querySelector(".input");
+
+Btn.addEventListener("click",() =>{
+	Input.focus();
+});
